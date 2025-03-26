4b795c9996fa7e453ed75d8eb5c78991c58bfcca
Patrick Her <renfu.her@gmail.com>
2025-03-26 23:07:32 +0800

    新增首頁服務相關API路由，整合至現有路由群組中，以支持首頁服務的獲取功能，並移除重複的路由定義，提升代碼整潔性。

--- routes/api.php
+++ routes/api.php
@@ -36,29 +36,33 @@ Route::prefix('auth')->group(function () {
 Route::prefix('v1')->name('api.v1.')->group(function () {
     // 關於我們
     Route::get('about', [AboutController::class, 'index'])->name('about');
-    
+
     // 聯絡資訊
     Route::get('contact', [ContactController::class, 'index'])->name('contact');
-    
+
     // 服務項目
     Route::get('services', [ServiceController::class, 'index'])->name('services');
     Route::get('services/{service}', [ServiceController::class, 'show'])->name('services.show');
-    
+
     // 合作花絮
     Route::get('highlights', [SchoolHighlightController::class, 'index'])->name('highlights');
     Route::get('highlights/{highlight}', [SchoolHighlightController::class, 'show'])->name('highlights.show');
-    
+
     // 合作學校
     Route::get('schools', [SchoolController::class, 'index'])->name('schools');
     Route::get('schools/{school}', [SchoolController::class, 'show'])->name('schools.show');
-    
+
     // 學生
     Route::get('students', [StudentController::class, 'index'])->name('students');
     Route::get('students/{student}', [StudentController::class, 'show'])->name('students.show');
-    
+
     // 會員
     Route::get('members', [MemberController::class, 'index'])->name('members');
     Route::get('members/{member}', [MemberController::class, 'show'])->name('members.show');
+
+    // Home Services
+    Route::get('/home-services', [HomeServiceController::class, 'index']);
+    Route::get('/home-services/{id}', [HomeServiceController::class, 'show']);
 });
 
 // 學生相關路由
@@ -67,7 +71,3 @@ Route::middleware('auth:sanctum')->group(function () {
     Route::post('/students/{student}/toggle', [App\Http\Controllers\Api\StudentController::class, 'toggleStudent']);
     Route::get('/students/selected', [App\Http\Controllers\Api\StudentController::class, 'getSelectedStudents']);
 });
-
-// Home Services
-Route::get('/home-services', [HomeServiceController::class, 'index']);
-Route::get('/home-services/{id}', [HomeServiceController::class, 'show']); 
