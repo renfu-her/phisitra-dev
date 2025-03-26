82b98dac150fdc5077ae392016c981a5f8462f30
Patrick Her <renfu.her@gmail.com>
2025-03-26 23:08:56 +0800

    在首頁控制器中新增調試代碼，以顯示首頁服務資料，便於開發過程中的問題排查。

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -30,6 +30,8 @@ class HomeController extends Controller
         $homeServicesData = $homeServicesResponse->json();
         $homeServices = $homeServicesData['data'] ?? [];
 
+        dd($homeServices);
+
         return view('home', compact('setting', 'about', 'banners', 'schools', 'homeServices'));
     }
 
