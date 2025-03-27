120d8b1634b3a6c2ec542aea7f9133240cf7a4ab
Patrick Her <renfu.her@gmail.com>
2025-03-27 09:46:25 +0800

    在學生控制器中新增調試功能，暫時顯示API回應以便於除錯。

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -21,6 +21,9 @@ class StudentController extends Controller
     {
 
         $response = Http::withoutVerifying()->get($this->apiUrl . '/students/' . $student->id)->json();
+        
+        dd($response, $this->apiUrl . '/students/' . $student->id);
+        
         $student = $response['data'];
 
         return view('students.show', compact('student'));
