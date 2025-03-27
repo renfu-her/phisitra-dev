da79b3554adb4961fccf789465209e0ed5c5d120
Patrick Her <renfu.her@gmail.com>
2025-03-27 09:38:46 +0800

    修正學生控制器中的顯示邏輯，將調試功能移至正確位置，以便於正確顯示學生資料。

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -19,10 +19,11 @@ class StudentController extends Controller
 
     public function show(Student $student)
     {
-        $response = Http::withoutVerifying()->get($this->apiUrl . '/students/' . $student->id)->json();
-        $student = $response['data'];
 
         dd($student);
+        
+        $response = Http::withoutVerifying()->get($this->apiUrl . '/students/' . $student->id)->json();
+        $student = $response['data'];
 
         return view('students.show', compact('student'));
     }
