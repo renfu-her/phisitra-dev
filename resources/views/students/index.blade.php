00f687c946a6276d4c8e0566ada12d8734b6e7f8
Patrick Her <renfu.her@gmail.com>
2025-03-27 09:31:36 +0800

    更新學生列表頁面，將標題改為「學生專區」並新增描述，使用 @forelse 來處理學生資料顯示，若無資料則顯示提示訊息。

--- resources/views/students/index.blade.php
+++ resources/views/students/index.blade.php
@@ -3,13 +3,16 @@
 @section('content')
 <div class="container py-4">
     <div class="row">
-        <div class="col-12">
-            <h1 class="mb-4">學生列表</h1>
+        <div class="section-title-wrapper">
+            <div class="section-title">
+                <h3>學生專區</h3>
+                <p>我們的優秀學生</p>
+            </div>
         </div>
     </div>
 
     <div class="row">
-        @foreach($students as $student)
+        @forelse($students as $student)
             <div class="col-md-3 col-sm-6">
                 <div class="card student-card">
                     <a href="{{ route('students.show', $student['id']) }}" class="text-decoration-none">
@@ -29,6 +32,12 @@
                             @endif
                         </div>
                     </a>
+                </div>
+            </div>
+        @empty
+            <div class="col-12">
+                <div class="alert alert-info">
+                    目前沒有任何學生資料。
                 </div>
             </div>
         @endforeach
