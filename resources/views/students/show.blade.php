a097009d4753840c0e1c401de6a4dc191f820072
Patrick Her <renfu.her@gmail.com>
2025-03-27 09:54:56 +0800

    優化學生詳細資料頁面，調整HTML結構與樣式，並修正條件判斷的格式以提升可讀性。

--- resources/views/students/show.blade.php
+++ resources/views/students/show.blade.php
@@ -1,15 +1,12 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container py-4">
-    @if($student['status'])
+    <div class="container py-4">
         <div class="row">
             <div class="col-md-4">
                 <div class="card">
-                    <img src="{{ Storage::url($student['data']['photo']) }}" 
-                         class="card-img-top student-photo" 
-                         alt="{{ $student['data']['name_zh'] }}"
-                         onerror="this.src='/images/default-student.jpg'">
+                    <img src="{{ Storage::url($student['data']['photo']) }}" class="card-img-top student-photo"
+                        alt="{{ $student['data']['name_zh'] }}" onerror="this.src='/images/default-student.jpg'">
                 </div>
             </div>
             <div class="col-md-8">
@@ -24,7 +21,7 @@
                             <p><strong>生日：</strong>{{ $student['data']['birth_date'] ?? '未提供' }}</p>
                             <p><strong>國籍：</strong>{{ $student['data']['nationality'] ?? '未提供' }}</p>
                             <p><strong>護照號碼：</strong>{{ $student['data']['passport_no'] ?? '未提供' }}</p>
-                            @if($student['data']['overseas_address'])
+                            @if ($student['data']['overseas_address'])
                                 <p><strong>海外地址：</strong>{{ $student['data']['overseas_address'] }}</p>
                             @endif
                         </div>
@@ -41,41 +38,38 @@
                     </div>
                 </div>
 
-                @if($student['data']['specialties'])
-                <div class="mb-3">
-                    <h5>專長</h5>
-                    <p>{{ $student['data']['specialties'] }}</p>
-                </div>
+                @if ($student['data']['specialties'])
+                    <div class="mb-3">
+                        <h5>專長</h5>
+                        <p>{{ $student['data']['specialties'] }}</p>
+                    </div>
                 @endif
 
-                @if($student['data']['remarks'])
-                <div class="mb-3">
-                    <h5>備註</h5>
-                    <p>{{ $student['data']['remarks'] }}</p>
-                </div>
+                @if ($student['data']['remarks'])
+                    <div class="mb-3">
+                        <h5>備註</h5>
+                        <p>{{ $student['data']['remarks'] }}</p>
+                    </div>
                 @endif
             </div>
         </div>
-    @else
-        <div class="alert alert-danger">
-            {{ $student['message'] }}
-        </div>
-    @endif
-</div>
+    </div>
 
-@push('styles')
-<style>
-    .student-photo {
-        height: 400px;
-        object-fit: cover;
-    }
-    h5 {
-        color: #333;
-        margin-bottom: 1rem;
-    }
-    .card {
-        box-shadow: 0 2px 4px rgba(0,0,0,0.1);
-    }
-</style>
-@endpush
-@endsection 
+    @push('styles')
+        <style>
+            .student-photo {
+                height: 400px;
+                object-fit: cover;
+            }
+
+            h5 {
+                color: #333;
+                margin-bottom: 1rem;
+            }
+
+            .card {
+                box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+            }
+        </style>
+    @endpush
+@endsection
